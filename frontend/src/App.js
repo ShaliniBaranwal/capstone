cf96ad494b7ec0478caf41208f1a93162fa9967b
Shalini Baranwal <shaliniblr@gmail.com>
2023-01-25 16:33:30 +0530

    updated frontend as per mockup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import Navbar from './components/Navbar/Navbar';
+import JobsinDomain from './components/JobsinDomain/JobsinDomain'
+import AppliedJobs from './components/AppliedJobs/AppliedJobs'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route element={<Dashboard/>} path="/Dashboard"></Route>
+          <Route element={<Dashboard />} path="/Dashboard"></Route>
+          <Route element={<JobsinDomain />} path="/JobsinDomain"></Route>
+          <Route element={<AppliedJobs />} path="/AppliedJobs"></Route>
         </Routes>
-      </Router>      
+      </Router>
     </div>
   );
 }
